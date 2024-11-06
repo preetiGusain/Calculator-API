b4800af2b885ec3e5df0f36965caf11b5ff95855
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-11-06 12:05:03 +0530

    Added logic for basic math operations

--- index.js
+++ index.js
@@ -1,20 +1,69 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-// Middleware to parse JSON in the request body, This middleware enables Express to parse JSON data in req.body.
 app.use(express.json());
+// Middleware to parse JSON in the request body, This middleware enables Express to parse JSON data in req.body.
 
-function calculateSum(n) {
-    let ans = 0;
-    return ans + Number(n);
+let currentResult = 0;
+
+function calculate(num1, num2, operator) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return (num1 / num2).toFixed(2);
+        case '%':
+            return num1 % num2;
+        default:
+            return 'Error: Invalid operator';
+    }
 }
 
+app.post('/add', (req, res) => {
+    const { num1, num2 } = req.body;
+    currentResult = calculate(num1, num2, '+');
+    res.send(currentResult.toString());
+});
 
-app.post('/', (req, res) => {
-    const n = req.body.n;
-    const ans = calculateSum(n)
-    res.send(ans.toString());
+app.post('/subtract', (req, res) => {
+    const { num1, num2 } = req.body;
+    currentResult = calculate(num1, num2, '-');
+    res.send(currentResult.toString());
+});
+
+app.post('/multiply', (req, res) => {
+    const { num1, num2 } = req.body;
+    currentResult = calculate(num1, num2, '*');
+    res.send(currentResult.toString());
+});
+
+app.post('/divide', (req, res) => {
+    const { num1, num2 } = req.body;
+    currentResult = calculate(num1, num2, '/');
+    res.send(currentResult.toString());
+});
+
+app.post('/mod', (req, res) => {
+    const { num1, num2 } = req.body;
+    currentResult = calculate(num1, num2, '%');
+    res.send(currentResult.toString());
+});
+
+app.get('/AC', (req, res) => {
+    currentResult = 0;
+    res.send(currentResult.toString());
+})
+
+// GET endpoint to retrieve the current result
+app.post('/=', (req, res) => {
+    res.send(currentResult.toString());
 });
 
 //Starting the server
